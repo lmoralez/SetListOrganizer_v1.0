c8722dfa0dafcfd6c45d79df1dc67c50dcab10a4
lmoralez <lmoralez2026@gmail.com>
2025-10-08 05:21:01 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
-// Electron main process placeholder
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1000,
+    height: 700,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  });
+
+  // Load your app’s main HTML file
+  win.loadFile('index.html');
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit();
+});
